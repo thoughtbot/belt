4c9288a07bce38db36db7bb2ccf8137048e9321a
Stephen Hanson <shanson@thoughtbot.com>
2023-05-05 14:08:06 -0500

    Switch to typescript

--- react-native/react-native.ts
+++ react-native/react-native.ts
@@ -4,7 +4,7 @@ console.log("React Native 🎉");
 
 program
   .description('Perform React Native and Expo setup and redundant tasks without your pants falling down!')
-  .command("eslint", "Configure ESLint", { executableFile: "eslint.js" })
-  .command("prettier", "Configure Prettier", { executableFile: "prettier.js" });
+  .command("eslint", "Configure ESLint", { executableFile: "eslint.ts" })
+  .command("prettier", "Configure Prettier", { executableFile: "prettier.ts" });
 
 program.parse();
