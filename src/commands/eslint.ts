14782af426ba7ce0f38841aa3483a38f71ca1ab0
William Larry <wlarry@thoughtbot.com>
2023-08-18 13:06:57 -0400

    Add eslint path (#15)
    * Add eslint path
    
    * Run linter
    
    * Remove log
    
    * Remove extra formatting file
    
    * Update to use thoughtbot eslint package
    
    * Remove overrides in template

--- src/commands/eslint.ts
+++ src/commands/eslint.ts
@@ -1,3 +1,39 @@
-export default function runEslint() {
-  console.log('ESLint!');
+import { log } from 'console';
+import path from 'path';
+import { fileURLToPath, URL } from 'url';
+import chalk from 'chalk';
+import * as eta from 'eta';
+import fs from 'fs-extra';
+import addDependency from '../util/addDependency';
+import getProjectDir from '../util/getProjectDir';
+import isEslintConfigured from '../util/isEslintConfigured';
+import isPackageInstalled from '../util/isPackageInstalled';
+import writeFile from '../util/writeFile';
+
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default async function runEslint() {
+  const projectDir = await getProjectDir();
+
+  if (await isEslintConfigured()) {
+    log('eslint config already exists');
+  } else {
+    const hasTypeScript = await isPackageInstalled('typescript');
+
+    const eslintConfigTemplate = await fs.readFile(
+      path.join(dirname, 'templates', 'eslintrc.js.eta'),
+    );
+
+    const fileContents = eta.render(eslintConfigTemplate.toString(), {
+      typescript: hasTypeScript,
+    });
+
+    await writeFile(path.join(projectDir, '.eslintrc.js'), fileContents, {
+      format: true,
+    });
+
+    await addDependency('@thoughtbot/eslint-config', { dev: true });
+
+    log(chalk.green('🎉 ESLint successfully configured'));
+  }
 }
---
+++ src/util/isEslintConfigured.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import fsExtra from 'fs-extra';
+import getProjectDir from './getProjectDir';
+import readPackageJson from './readPackageJson';
+
+export default async function isEslintConfigured() {
+  const packageJson = await readPackageJson();
+
+  const projectDir = await getProjectDir();
+
+  const hasEslintConfigInPackageJson = Object.prototype.hasOwnProperty.call(
+    packageJson,
+    'eslint',
+  );
+  const hasEslintrcJsFile = await fsExtra.exists(
+    path.join(projectDir, '.eslintrc.js'),
+  );
+  const hasEslintJsonFile = await fsExtra.exists(
+    path.join(projectDir, '.eslintrc.json'),
+  );
+
+  return hasEslintConfigInPackageJson || hasEslintrcJsFile || hasEslintJsonFile;
+}
