4543d928edc8727a7bfa1e918b3b7c2a7bd8f990
Stephen Hanson <shanson@thoughtbot.com>
2023-11-10 18:48:11 -0600

    Fix some issues

--- bin/checks/build-with-latest-expo.check.js
+++ bin/checks/build-with-latest-expo.check.js
@@ -1,19 +1,28 @@
 #!/usr/bin/env node
 import { execSync } from 'child_process';
 import fs from 'fs-extra';
-import path from 'path';
-import { PACKAGE_ROOT } from '../../src/constants';
 
 async function runCheck() {
-  const dir = path.join(PACKAGE_ROOT, 'builds');
+  const dir = './builds';
+
+  // build to /dist
   execSync('yarn build', { stdio: 'inherit' });
+
+  // clean /builds, cd into it
   fs.rmSync(dir, { recursive: true, force: true });
   fs.mkdirSync(dir, { recursive: true });
   process.chdir(dir);
+
+  // run CLI
   execSync('node ../dist/index.js ExpoSample --no-interactive --is-test', {
     stdio: 'inherit',
   });
-  execSync('yarn test:all', { stdio: 'inherit' });
+
+  process.chdir('./ExpoSample');
+
+  // verify linter and tests all pass in new project
+  const pkgMgr = fs.existsSync('package-lock.json') ? 'npm run' : 'yarn';
+  execSync(`${pkgMgr} test:all`, { stdio: 'inherit' });
 }
 
 void runCheck();
