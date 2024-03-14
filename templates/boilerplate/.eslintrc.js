4376f49e20a38f70c2ea8f25015d06d0ffcfc015
Diego Oliveira <diego@thoughtbot.com>
2024-03-14 12:57:11 -0300

    Remove plugins from eslint file and add importing rules, it also fix import order in the boilerplate

--- templates/boilerplate/.eslintrc.js
+++ templates/boilerplate/.eslintrc.js
@@ -4,4 +4,24 @@ module.exports = {
     '@thoughtbot/eslint-config/native',
     '@thoughtbot/eslint-config/typescript',
   ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
