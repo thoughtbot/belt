b4e8a3f684b21447ad9cf2d1a8f914e09d860152
Adrian Ross <adriantoddross@gmail.com>
2024-03-27 09:43:20 -0700

    Replace SearchScreen import with HomeScreen

--- templates/reactNavigationBottomTabs/src/navigators/DashboardStack.tsx
+++ templates/reactNavigationBottomTabs/src/navigators/DashboardStack.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import SearchScreen from '../screens/SearchScreen/SearchScreen';
+import HomeScreen from 'src/screens/HomeScreen/HomeScreen';
 import InformationScreen from '../screens/InformationScreen/InformationScreen';
 import { DashboardStackParamList } from './navigatorTypes';
 
@@ -9,7 +9,7 @@ const Dashboard = createNativeStackNavigator<DashboardStackParamList>();
 export default function DashboardStack() {
   return (
     <Dashboard.Navigator>
-      <Dashboard.Screen name="Home" component={SearchScreen} />
+      <Dashboard.Screen name="Home" component={HomeScreen} />
       <Dashboard.Screen name="Information" component={InformationScreen} />
     </Dashboard.Navigator>
   );
