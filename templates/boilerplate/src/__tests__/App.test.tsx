675b92218ee9851083fdc65aa2af4a120994de09
Adrian Ross <adriantoddross@gmail.com>
2024-03-27 09:43:29 -0700

    Fix Jest errors

--- templates/boilerplate/src/__tests__/App.test.tsx
+++ templates/boilerplate/src/__tests__/App.test.tsx
@@ -3,6 +3,7 @@ import RootNavigator from 'src/navigators/RootNavigator';
 import render from 'src/test/render';
 
 test('renders', async () => {
+  jest.useFakeTimers();
   render(<RootNavigator />);
   expect(await screen.findByText(/Open up App.tsx/)).toBeDefined();
 });
