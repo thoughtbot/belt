cecd31f9ffafd934c7a5edd71cb8679eb83ca413
Tomi Alu <tomi@thoughtbot.com>
2024-10-25 15:40:00 +0100

    test: assert that function is called with right arguments

--- src/util/print/__tests__/printWelcome.test.ts
+++ src/util/print/__tests__/printWelcome.test.ts
@@ -1,8 +1,30 @@
-import { test, vi } from 'vitest';
+import { test, vi, describe, expect, beforeEach } from 'vitest';
+import chalk from 'chalk';
 import printWelcome from '../printWelcome';
 
-vi.mock('../../print', () => ({ default: vi.fn() }));
+const consoleSpy = vi.spyOn(console, 'log');
 
-test('doesnt error', () => {
-  printWelcome();
+describe('printWelcome', () => {
+  beforeEach(() => {
+    consoleSpy.mockClear();
+  });
+
+  test('prints introduction message', () => {
+    process.argv = ['node', 'script.js', 'create'];
+    printWelcome();
+    expect(console.log).toHaveBeenCalledWith(chalk.bold('\n\n\t👖 Belt 👖\n'));
+  });
+
+  describe('does not print introduction message for version', () => {
+    test('works for --version', () => {
+      process.argv = ['node', 'script.js', '--version'];
+      printWelcome();
+      expect(consoleSpy.mock.calls.length).toBe(0);
+    });
+    test('works for -V', () => {
+      process.argv = ['node', 'script.js', '-V'];
+      printWelcome();
+      expect(consoleSpy.mock.calls.length).toBe(0);
+    });
+  });
 });
