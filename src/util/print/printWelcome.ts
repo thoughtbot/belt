990d3fa79fbb8f000451fb829ce6ca73b1fd13b1
Tomi Alu <tomi@thoughtbot.com>
2024-10-25 15:17:19 +0100

    feat(command): return package version for version option
    Co-authored-by: Rakesh Arunachalam <rakeshpetit@gmail.com>

--- src/util/print/printWelcome.ts
+++ src/util/print/printWelcome.ts
@@ -2,6 +2,11 @@ import chalk from 'chalk';
 import print from '../print';
 
 export default function printWelcome() {
+  const showVersionCommand =
+    process.argv.includes('--version') || process.argv.includes('-V');
+  if (showVersionCommand) {
+    return;
+  }
   print(chalk.bold('\n\n\t👖 Belt 👖\n'));
   print(
     'Perform project setup and redundant tasks\n    without your pants falling down!\n\n',
