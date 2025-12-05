c68360dc71dca3383ed0828ee9d66d66d83277da
Rakesh Arunachalam <rakesh@thoughtbot.com>
2025-12-05 14:53:34 +0000

    Upgrade Belt CLI to use Expo 54 (#74)
    [Expo 54](https://expo.dev/changelog/sdk-54) is the latest release of
    SDK and has been around for a few months now. This PR is an attempt to
    upgrade the Belt CLI to use Expo 54.
    
    Co-authored by: @MalcolmTomisin 
    
    ## How to test
    
    Checkout this branch and run the commands below:
    ```
    // clear existing builds and belt build
    rm -rf dist builds
    // Generate Belt CLI build
    yarn build 
    // Generate new ExpoSample app using the Belt CLI
    node bin/checks/build-with-latest-expo.check.js
    ```

--- templates/boilerplate/jest.config.js
+++ templates/boilerplate/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules', 'src/test'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native-community|@react-native|react-native|@react-navigation)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!((jest-)?react-native|@react-native-community|@react-native|react-native|@react-navigation)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|until-async)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
