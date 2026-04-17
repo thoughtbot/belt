53c335c5ce7d74c850be6d9c958da95d3123f5e7
Rakesh Arunachalam <rakesh@thoughtbot.com>
2026-04-17 12:02:13 +0100

    fix: jest config for msw

--- templates/boilerplate/jest.config.js
+++ templates/boilerplate/jest.config.js
@@ -3,8 +3,11 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules', 'src/test'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native-community|@react-native|react-native|@react-navigation)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|until-async)',
+    'node_modules/(?!((jest-)?react-native|@react-native-community|@react-native|react-native|@react-navigation)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|until-async|@open-draft|rettime)',
   ],
+  transform: {
+    '^.+\\.mjs$': 'babel-jest',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '.+\\.(png|jpg|ttf|woff|woff2)$': '<rootDir>/src/test/fileMock.js',
